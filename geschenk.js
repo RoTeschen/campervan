e0cc63382391ae787c106b50f7a062a6ec52b3c9
RoTeschen <86953756+RoTeschen@users.noreply.github.com>
2022-02-06 20:47:28 +0100

    Delete geschenk.js

--- geschenk.js
+++
@@ -1,9 +0,0 @@
-$(".box1").click(function(){
-  $(".box1").css("background-color", "#C84B31");
-  $(".pbox1").text("Schade, hier gibt es leider kein Geschenk");
-});
-
-$(".box2").click(function(){
-  $(".box2").css("background-color", "#C84B31");
-  $(".pbox2").text("Schade, hier gibt es leider kein Geschenk");
-});
